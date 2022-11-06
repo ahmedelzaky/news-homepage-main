56f56abcf518e44330125bc84f5d85ae201b2aed
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2022-11-06 16:32:16 +0200

    version 1.1

--- menu.js
+++ menu.js
@@ -1,12 +1,12 @@
-let mButton = document.querySelector(".mobile-menu");
-let menu = document.querySelector(".overlay");
+// let mButton = document.querySelector(".mobile-menu");
+// let menu = document.querySelector(".overlay");
 
-mButton.addEventListener("click", function () {
-  menu.style.display = "block";
-});
+// mButton.addEventListener("click", function () {
+//   menu.style.display = "block";
+// });
 
-let cButton = document.querySelector("#exit");
+// let cButton = document.querySelector("#exit");
 
-cButton.addEventListener("click", function () {
-  menu.style.display = "none";
-});
+// cButton.addEventListener("click", function () {
+//   menu.style.display = "none";
+// });
